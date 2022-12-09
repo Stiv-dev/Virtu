a2dc04877241989992058d060255aec79e9d4b81
Jean-Pascal MEWENEMESSE <scalpacademy.io@gmail.com>
2022-12-09 12:20:02 +0100

    add set env

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+
+
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=admin
+export POSTGRES_PASSWORD=admin1234
+export POSTGRES_HOST=udagramd-db
+export POSTGRES_DB=udagramdb
+export AWS_BUCKET=udagram-esiea-8765
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
